fb0df21873614bd16a4d271d24f8d2f321dfdd6a
pancake0003 <songyuemmm@gmail.com>
2025-08-20 01:32:34 -0700

    fuck next config man

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,17 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'cdn.jsdelivr.net' },
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: 'cdn.jsdelivr.net' }],
   },
   async rewrites() {
     if (process.env.NODE_ENV === 'production') {
       return [
+        // manifest
         {
-          source: '/gacha/:path*',
+          source: '/gacha/manifest.json',
           destination:
-            'https://cdn.jsdelivr.net/gh/Gracelu128/pjsk-sim@main/my-app/public/gacha/:path*',
+            'https://cdn.jsdelivr.net/gh/Gracelu128/pjsk-sim@main/my-app/public/gacha/manifest.json',
+        },
+        // logos
+        {
+          source: '/gacha/:gid/logo/:file*',
+          destination:
+            'https://cdn.jsdelivr.net/gh/Gracelu128/pjsk-sim@main/my-app/public/gacha/:gid/logo/:file*',
+        },
+        // backgrounds & overlays
+        {
+          source: '/gacha/:gid/screen/texture/:file*',
+          destination:
+            'https://cdn.jsdelivr.net/gh/Gracelu128/pjsk-sim@main/my-app/public/gacha/:gid/screen/texture/:file*',
+        },
+        // banners
+        {
+          source: '/gacha/:gid/banner/:file*',
+          destination:
+            'https://cdn.jsdelivr.net/gh/Gracelu128/pjsk-sim@main/my-app/public/gacha/:gid/banner/:file*',
         },
       ];
     }
