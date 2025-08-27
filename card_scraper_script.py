f8af8a6fa30fefc4595a1279658d4b78982717ef
Grace Lu <lu986@purdue.edu>
2025-08-27 19:55:33 -0400

    Made inputs of GHA optional

--- card_scraper_script.py
+++ card_scraper_script.py
@@ -3,10 +3,14 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--start_card", type=int, required=False)
-    parser.add_argument("--end_card", type=int, required=False)
+    parser.add_argument("--start_card", type=int, required=True)
+    parser.add_argument("--end_card", type=int, required=True)
     parser.add_argument("--task", choices=["scrape card info", "reorder card info"])
     args = parser.parse_args()
+
+    if args.start_card is None or args.end_card is None:
+        print("Skipping card scraping: start_card or end_card not provided.")
+        return
 
     if args.task == "scrape card info":
         scrape_card_images(start_num=args.start_card, end_num=args.end_card)
--- gacha_scraper_script.py
+++ gacha_scraper_script.py
@@ -3,13 +3,17 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--start_gacha", type=int, required=False)
-    parser.add_argument("--end_gacha", type=int, required=False)
+    parser.add_argument("--start_gacha", type=int, required=True)
+    parser.add_argument("--end_gacha", type=int, required=True)
     parser.add_argument("--task", choices=["scrape gacha info", "reorder gacha info"])
     args = parser.parse_args()
 
+    if args.start_gacha is None or args.end_gacha is None:
+        print("Skipping gacha scraping: start_gacha or gacha_card not provided.")
+        return
+
     if args.task == "scrape gacha info":
-        pass
+        sekaibest_scrape_gacha_info(start_gacha=args.start_gacha, end_gacha=args.end_gacha)
     elif args.task == "reorder gacha info":
         desired_gacha_metadata_order = [
             "id",
@@ -21,7 +25,7 @@ def main():
             "featured_cards"
         ]
         json_reorder("my-app/src/data/gacha_metadata.json", desired_gacha_metadata_order)
-        split_card_metadata()
+        split_gacha_metadata()
     
 if __name__ == "__main__":
     main()
